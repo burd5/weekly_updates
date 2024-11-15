dc154620e989558a5823eabe0995de43add53fdf
Austin Burdette <austinburdette5@gmail.com>
2024-11-14 19:30:59 -0500

    mapped out api call and params for events with Ticketmaster API

--- src/events.py
+++ src/events.py
@@ -1,4 +1,8 @@
 from datetime import datetime
+import requests
+import os
+
+api_key = os.environ.get("ticketmaster_apikey")
 
 class Event:
     def __init__(self, name, venue, location, frequency, start_date, end_date):
@@ -9,3 +13,13 @@ class Event:
         self.start_date = start_date
         self.end_date = end_date
         self.created_at = datetime.now()
+
+    @classmethod
+    def find_events(cls, name, venue, location, start_date, end_date):
+        response = requests.get(
+            f"""https://app.ticketmaster.com/discovery/v2/attractions.json?keyword={name}&venue={venue} 
+                &location={location}&startDateTime={start_date}&endDateTime={end_date}apikey={api_key}"""
+        )
+        data = response.json()
+
+        return data
